0a3ade62738161662f5e084ad07ec2d54410e39c
hyeonji91 <hjkim1770@gmail.com>
2025-11-11 10:11:56 +0900

    feature #19 : 비관적 락 삭제

--- src/main/java/com/irum/productservice/domain/product/domain/repository/ProductOptionValueRepository.java
+++ src/main/java/com/irum/productservice/domain/product/domain/repository/ProductOptionValueRepository.java
@@ -17,12 +17,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ProductOptionValueRepository extends JpaRepository<ProductOptionValue, UUID> {
 
-    /** 락 획득까지 최대 3초 대기 TODO : 대기 시간 정책 정하기 */
-    @Lock(LockModeType.PESSIMISTIC_WRITE)
-    @QueryHints({@QueryHint(name = "jakarta.persistence.lock.timeout", value = "3000")})
-    @Query("select pov from ProductOptionValue pov where pov.id in :ids")
-    List<ProductOptionValue> findAllByIdInWithLock(@Param("ids") List<UUID> ids);
-
     @Query("select pov from ProductOptionValue pov where pov.id in :ids")
     List<ProductOptionValue> findAllByIds(@Param("ids") List<UUID> ids);
 
@@ -30,19 +24,6 @@ public interface ProductOptionValueRepository extends JpaRepository<ProductOptio
 
     List<ProductOptionValue> findAllByOptionGroup_Product(Product product);
 
-    /** 기본적으로 fetch join은 락에 안잡힘 */
-    @Lock(LockModeType.PESSIMISTIC_WRITE)
-    @QueryHints({@QueryHint(name = "jakarta.persistence.lock.timeout", value = "3000")})
-    @Query(
-            """
-    SELECT pov
-    FROM ProductOptionValue pov
-    JOIN FETCH pov.optionGroup og
-    JOIN FETCH og.product p
-    JOIN FETCH p.store s
-    WHERE pov.id IN :ids
-    """)
-    List<ProductOptionValue> findAllByIdWithLockFetchJoin(@Param("ids") List<UUID> ids);
 
     @Query(
             """
