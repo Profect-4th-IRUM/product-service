1e6ee6bc8664ca9c53f4e8f48c4293f13a78ab12
psy <psy915@swu.ac.kr>
2025-11-17 02:45:07 +0900

    chore #39: Order-service FeignClient 메서드명 수정

--- src/main/java/com/irum/openfeign/client/OrderClient.java
+++ src/main/java/com/irum/openfeign/client/OrderClient.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "order-service")
+@FeignClient(name = "ORDER-SERVICE")
 public interface OrderClient {
 
     @GetMapping("/internal/order-details/{orderDetailId}")
