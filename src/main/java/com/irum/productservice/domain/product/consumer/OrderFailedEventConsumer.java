f6a9b4847f3e76682a49c8ce0f0804da385b7a9c
hyeonji91 <hjkim1770@gmail.com>
2025-12-05 20:12:59 +0900

    feature #75 : JsonDeserializer  -> StringDeserializer

--- src/main/java/com/irum/productservice/domain/product/consumer/OrderFailedEventConsumer.java
+++ src/main/java/com/irum/productservice/domain/product/consumer/OrderFailedEventConsumer.java
@@ -1,7 +1,11 @@
 package com.irum.productservice.domain.product.consumer;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.irum.global.advice.exception.CommonException;
 import com.irum.productservice.domain.product.Internal.service.ProductInternalService;
 import com.irum.productservice.domain.product.event.OrderFailedEvent;
+import com.irum.productservice.global.exception.errorcode.GlobalErrorCode;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.DltHandler;
@@ -19,6 +23,7 @@ import org.springframework.stereotype.Component;
 @Slf4j
 public class OrderFailedEventConsumer {
     private final ProductInternalService productInternalService;
+    private final ObjectMapper objectMapper;
 
     @RetryableTopic(
             attempts = "3",
@@ -28,15 +33,26 @@ public class OrderFailedEventConsumer {
     @KafkaListener(
             topics = "${spring.kafka.topics.order-failed}",
             groupId = "${spring.kafka.consumer.group-id}")
-    public void handleOrderFailed(OrderFailedEvent event) {
-        log.info("[외부] Order Failed event 수신 완료 {}", event);
+    public void handleOrderFailed(String message) {
+        log.info("[외부] Order Failed event 수신 완료 {}", message);
 
-        productInternalService.rollbackStock(event);
+        try {
+            // String -> DTO 클래스 수동 변환
+            OrderFailedEvent event = objectMapper.readValue(message, OrderFailedEvent.class);
+            log.info("[변환] 객체 변환 성공");
+
+            productInternalService.rollbackStock(event);
+        } catch (JsonProcessingException e) {
+            log.error("[변환] String -> DTO class 변환 실패 (JSON 형식이 안 맞음): message = {}, errorclass = {}, errormessage = {}", message, e.getClass(), e.getMessage());
+            throw new CommonException(GlobalErrorCode.JSON_PROCESSING_EXCEPTION);
+        }
+
+
     }
 
     @DltHandler // 재시도 기회를 모두 소진한 메시지가 DLQ에 도착하면, DLQ에서 메시지를 꺼내서 처리
     public void processDlt(
-            @Payload OrderFailedEvent event,
+            @Payload String rawMessage,
             @Header(KafkaHeaders.ORIGINAL_TOPIC) String originalTopic,
             @Header(KafkaHeaders.ORIGINAL_OFFSET) long originalOffset,
             @Header(KafkaHeaders.ORIGINAL_PARTITION) int originalPartition,
@@ -46,6 +62,17 @@ public class OrderFailedEventConsumer {
             @Header(KafkaHeaders.RECEIVED_PARTITION) int partition,
             @Header(KafkaHeaders.EXCEPTION_MESSAGE) String exception,
             @Header(KafkaHeaders.EXCEPTION_CAUSE_FQCN) String exceptionFQCN) {
+
+        OrderFailedEvent event = null;
+        try {
+
+            event = objectMapper.readValue(rawMessage, OrderFailedEvent.class);
+            log.error("DLT 파싱 성공");
+        } catch (JsonProcessingException e) {
+            log.error("[변환] String -> DTO class 변환 실패 (JSON 형식이 안 맞음): message = {}, errorclass = {}, errormessage = {}", rawMessage, e.getClass(), e.getMessage());
+            throw new CommonException(GlobalErrorCode.JSON_PROCESSING_EXCEPTION);
+        }
+
         log.error(
                 "원본 토픽 : {} \t 원본 오프셋 : {} \t 원본 파티션 : {} \t 원본 컨슈머 그룹 : {}",
                 originalTopic,
--- src/main/java/com/irum/productservice/global/exception/errorcode/GlobalErrorCode.java
+++ src/main/java/com/irum/productservice/global/exception/errorcode/GlobalErrorCode.java
@@ -10,7 +10,10 @@ import org.springframework.http.HttpStatus;
 public enum GlobalErrorCode implements BaseErrorCode {
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 오류입니다. 관리자에게 문의해주세요."),
     INVALID_CURSOR(HttpStatus.BAD_REQUEST, "리소스를 조회할 수 없습니다."),
-    EMPTY_REQUEST(HttpStatus.BAD_REQUEST, "비어있는 요청입니다.");
+    EMPTY_REQUEST(HttpStatus.BAD_REQUEST, "비어있는 요청입니다."),
+    JSON_PROCESSING_EXCEPTION(HttpStatus.INTERNAL_SERVER_ERROR, "json processing exception"),
+
+    ;
 
     private final HttpStatus httpStatus;
     private final String message;
